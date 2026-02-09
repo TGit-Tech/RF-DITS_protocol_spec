f6510f475d58852ffe760117245f7e8df831eae7
TGit-Tech <TGitTech@gmail.com>
2026-02-09 11:36:37 -0700

    Start Sketch

---
+++ rf_dits/rf_dits.ino
@@ -0,0 +1,22 @@
+/******************************************************************************************************************//**
+ * @file      rf_dits.ino
+ * @brief     Example Arduino Sketch for the RF-DITS protocol on wireless-UART Ebyte E22 Transceiver.
+ * @details   Main Sketch
+ * @version   2629 [YYMD]
+ * @bug       NA
+ * @note      See 
+ * @warning   See
+ * @copyright MIT Public License
+ * @todo      Wire and Test
+ * @author    TGIT-TECH   WIP 2629
+ **********************************************************************************************************************/
+#include "rf_dits.h"
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
